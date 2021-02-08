d759eb5c068d608a17dccc3c14f3935d8f51d104
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-02-08 18:20:42 -0500

    Initial commit of template and sample recipes

---
+++ PowerShell.ps1
@@ -0,0 +1,44 @@
+$Parameters = @{    
+    # Type of application (EXE or MSI)
+    'Type'                  = 'MSI'
+    
+    # Name of application publisher
+    'Publisher'             = 'Microsoft';
+    
+    # Name of application
+    'Name'                  = 'PowerShell';
+    
+    # Architecture of application (x64 or x86)
+    'Architecture'          = 'x64';
+    
+    # Name of the applicaton's uninstall registry key (EXE applications only)
+    #'RegKeyName'            = 'REGKEYNAME';
+    
+    # Does the application's uninstall registry key also contain the version number? ($true of $false) (EXE applications only)
+    #'RegKeyContainsVersion' = $false
+    
+    # Is the application 64-bit? ($true or $false)
+    'Is64bit'               = $true;
+    
+    # Estimated application installation time in minutes
+    'EstimatedRuntimeMins'  = '5';
+    
+    # Maximum application installation time in minutes
+    'MaximumRuntimeMins'    = '15';
+    
+    # Distribution point group name to distribute content to
+    'DPGroup'               = 'ALL';
+    
+    # Defines collections used to deploy the application to (PROD or TEST)
+    'Environment'           = 'PROD';
+    
+    # Defines if the application is "free" and should be available to all users ($true or $false)
+    'IsFreeApp'             = $true;
+    
+    # Run the script in verbose mode ($true or $false)
+    'Verbose'               = $true
+}
+
+Import-Module -FullyQualifiedName "..\CMPackager\CMPackager.psm1" -Force
+
+New-CMPApplication @Parameters
---
+++ _Template.ps1
@@ -0,0 +1,44 @@
+$Parameters = @{    
+    # Type of application (EXE or MSI)
+    'Type'                  = 'TYPE'
+    
+    # Name of application publisher
+    'Publisher'             = 'PUBLISHER';
+    
+    # Name of application
+    'Name'                  = 'NAME';
+    
+    # Architecture of application (x64 or x86)
+    'Architecture'          = 'x64';
+    
+    # Name of the applicaton's uninstall registry key (EXE applications only)
+    'RegKeyName'            = 'REGKEYNAME';
+    
+    # Does the application's uninstall registry key also contain the version number? ($true of $false) (EXE applications only)
+    'RegKeyContainsVersion' = $false
+    
+    # Is the application 64-bit? ($true or $false)
+    'Is64bit'               = $true;
+    
+    # Estimated application installation time in minutes
+    'EstimatedRuntimeMins'  = 'MIN';
+    
+    # Maximum application installation time in minutes
+    'MaximumRuntimeMins'    = 'MAX';
+    
+    # Distribution point group name to distribute content to
+    'DPGroup'               = 'ALL';
+    
+    # Defines collections used to deploy the application to (PROD or TEST)
+    'Environment'           = 'PROD';
+    
+    # Defines if the application is "free" and should be available to all users ($true or $false)
+    'IsFreeApp'             = $true;
+    
+    # Run the script in verbose mode ($true or $false)
+    'Verbose'               = $false
+}
+
+Import-Module -FullyQualifiedName "..\CMPackager\CMPackager.psm1" -Force
+
+New-CMPApplication @Parameters
